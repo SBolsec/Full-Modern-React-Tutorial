f9471683f17371a1692cc04d8be7333079e4d02f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-16 18:53:06 +0100

    props

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,9 +3,6 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.google.com";
 
   return (
     <div className="App">
