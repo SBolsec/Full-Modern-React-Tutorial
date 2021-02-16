7c96bb71d2fc6d4f163dd9f3ea4d9d678325d46e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-16 19:08:04 +0100

    functions as props

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -3,17 +3,21 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem impsum...', author: 'mario', id: 1},
-        { title: 'Welcome party!', body: 'lorem impsum...', author: 'yoshi', id: 2},
-        { title: 'Web dev top tips', body: 'lorem impsum...', author: 'mario', id: 3}
-    ]);    
-
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+  
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);
+    }
+  
     return (
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs!" />
-        </div>
+      <div className="home">
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      </div>
     );
-}
- 
-export default Home;
+  }
+   
+  export default Home;
