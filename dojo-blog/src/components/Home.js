b3dc275529f4627bb07f057ffb8a495d45c417cc
BuildTools <unconfigured@null.spigotmc.org>
2021-02-16 18:32:15 +0100

    click event

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Home = () => {
+    const handleClick = (e) => {
+        console.log(e)
+    }
+    const handleClickAgain = (e, name) => {
+        console.log(name, e.target)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain(e, 'mario')}>Click me again</button>
         </div>
     );
 }
