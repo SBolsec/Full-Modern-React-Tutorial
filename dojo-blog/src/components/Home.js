0ef15cdf945a889fdf56074bbd11cc1565ced68f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-16 19:36:34 +0100

    fetching data with useEffect

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -2,24 +2,18 @@ import React, { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-  
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs);
-    };
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log('hello')
+        fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(blogs => setBlogs(blogs))
+            .catch(err => console.error(err));
     }, []);
   
     return (
       <div className="home">
-        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+        {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       </div>
     );
 }
