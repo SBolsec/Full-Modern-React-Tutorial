cec5922a60789d31a75051f068a3f0443c44b0f4
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-02-18 14:01:43 +0100

    for merge

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
     </div>
   );
 }
